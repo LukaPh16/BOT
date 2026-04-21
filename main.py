cff7d20ae5a54778a3cdbcbd43168bbf6b531c40
LukaPh16 <lukaphirtskhalava16@gmail.com>
2026-04-21 16:28:18 +0400

    Added dB levels and now it listens until I am quiet

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
+import sys
 import whisper
 import pyaudio
 import numpy as np
 import audioop
+import time
 
-model = whisper.load_model("base", device = "cuda") 
+model = whisper.load_model("base", device = "cuda")  # remove cuda unless confirmed working
 
-RATE = 48000 #mic 
+RATE = 48000
 TARGET_RATE = 16000
-CHUNK_DURATION = 3  # seconds
-CHUNK_SIZE = int(RATE * CHUNK_DURATION)
-
-p = pyaudio.PyAudio()
+CHUNK_SIZE = 1024  # small = real-time
 
 DEVICE_INDEX = 4 #mic
+MIN_VOLUME = 65  # dB threshold (adjust later)
+DELAY = 1.5
+
+p = pyaudio.PyAudio()
 
 stream = p.open(
     format=pyaudio.paInt16,
@@ -20,18 +23,37 @@ stream = p.open(
     rate=RATE,
     input=True,
     input_device_index=DEVICE_INDEX,
-    frames_per_buffer=1024
+    frames_per_buffer=CHUNK_SIZE
 )
 
 print("Listening...")
 
-while True:
-    data = stream.read(CHUNK_SIZE, exception_on_overflow=False)
+def get_db(block):
+    data = np.frombuffer(block, dtype=np.int16).astype(np.float32)
+    rms = np.sqrt(np.mean(data**2))
 
-    data_16k, _ = audioop.ratecv(data, 2, 1, RATE, TARGET_RATE, None)
+    if rms > 0:
+        db = 20 * np.log10(rms)
+    else:
+        db = -100  # silence
 
-    audio_np = np.frombuffer(data_16k, np.int16).astype(np.float32) / 32768.0
+    return db
 
-    result = model.transcribe(audio_np, language="en")
 
-    print("You said:", result["text"])
+def db():
+    while True:
+        data = stream.read(CHUNK_SIZE, exception_on_overflow=False)
+
+        db = get_db(data)
+        print(f"Volume: {db:.2f} dB    ", end="\r")
+
+        if db > MIN_VOLUME:
+            print("Speaking detected!", end="\r")
+            time.sleep(DELAY)
+
+
+try:
+    db()
+
+except KeyboardInterrupt:
+    sys.exit(0)
